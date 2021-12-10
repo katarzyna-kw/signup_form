a6e56f0c61b5b1a544dd12f16be8b9abb06a5656
katarzyna_w <katarzyna.koch@gmail.com>
2021-12-10 08:16:55 -0600

    responsive bg imgs

--- src/App.js
+++ src/App.js
@@ -1,10 +1,14 @@
 import Card from './components/Card.component'
 import Footer from './components/Footer.component'
 import './App.css';
+import desktopBg from './img/bg-intro-desktop.png'
+import mobileBg from './img/bg-intro-mobile.png'
 
 function App() {
+  const bgUrl = window.innerWidth >= 600 ? desktopBg : mobileBg
+
   return (
-    <div className="App">
+    <div className="App" style={{backgroundImage: `url(${bgUrl})` }}>
       <Card />
       <Footer />
     </div>
