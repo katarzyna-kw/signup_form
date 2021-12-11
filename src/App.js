a26f031a0c32a0ebfab3df155bc7efc20b1ffc42
katarzyna_w <katarzyna.koch@gmail.com>
2021-12-11 15:08:44 -0600

    responsive bg img when user resizes window

--- src/App.js
+++ src/App.js
@@ -1,3 +1,4 @@
+import React, { useState, useEffect } from 'react'
 import Card from './components/Card.component'
 import Footer from './components/Footer.component'
 import './App.css';
@@ -5,7 +6,20 @@ import desktopBg from './img/bg-intro-desktop.png'
 import mobileBg from './img/bg-intro-mobile.png'
 
 function App() {
+
+  const [windowWidth, setWindowWidth] = useState(window.innerWidth);
   const bgUrl = window.innerWidth >= 600 ? desktopBg : mobileBg
+
+  useEffect(() => {
+    const updateBg = () => {
+      setWindowWidth(window.innerWidth);
+    }
+
+    window.addEventListener("resize", updateBg);
+    
+    return () => window.removeEventListener("resize", updateBg);
+    }, 
+  []);
 
   return (
     <div className="App" style={{backgroundImage: `url(${bgUrl})` }}>
