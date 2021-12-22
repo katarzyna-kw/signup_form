735e25549cc25c896b20e8405f71691cf7208d57
katarzyna_w <katarzyna.koch@gmail.com>
2021-12-22 10:05:34 -0600

    change h6 to h2 in HeaderText per rec

--- src/components/HeaderText.component.jsx
+++ src/components/HeaderText.component.jsx
@@ -5,9 +5,9 @@ export default function HeaderText() {
    return (
       <section className="text-container">
          <h1>Learn to code by watching others</h1>
-         <h6>
+         <h2>
          See how experienced developers solve problems in real-time. Watching scripted tutorials is great, but understanding how developers think is invaluable.
-         </h6>
+         </h2>
     </section>
    )
 }
