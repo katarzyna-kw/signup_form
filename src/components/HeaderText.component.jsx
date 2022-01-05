48e161c8c8673eb0d22c62f5e322f18fa2a359f5
katarzyna_w <katarzyna.koch@gmail.com>
2022-01-05 10:34:10 -0600

    check against prototype

--- src/components/HeaderText.component.jsx
+++ src/components/HeaderText.component.jsx
@@ -6,7 +6,7 @@ export default function HeaderText() {
       <section className="text-container">
          <h1>Learn to code by watching others</h1>
          <h2>
-         See how experienced developers solve problems in real-time. Watching scripted tutorials is great, but understanding how developers think is invaluable.
+            See how experienced developers solve problems in real-time. Watching scripted tutorials is great, but understanding how developers think is invaluable.
          </h2>
     </section>
    )
