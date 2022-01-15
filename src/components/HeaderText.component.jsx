a7a11ceb9bd9fd755b4e8e69b2a3be9ebb649986
katarzyna_w <katarzyna.koch@gmail.com>
2022-01-15 11:36:31 -0600

    refactor HeaderText with conventions --

--- src/components/HeaderText.component.jsx
+++ src/components/HeaderText.component.jsx
@@ -3,9 +3,9 @@ import './HeaderText.css'
 
 export default function HeaderText() {
    return (
-      <section className="header--container">
-         <h1 className="header--title">Learn to code by watching others</h1>
-         <h2 className="header--subhead">
+      <section className="header__container">
+         <h1 className="header__title">Learn to code by watching others</h1>
+         <h2 className="header__subhead">
             See how experienced developers solve problems in real-time. Watching scripted tutorials is great, but understanding how developers think is invaluable.
          </h2>
     </section>
